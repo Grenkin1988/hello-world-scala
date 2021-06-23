817c392e80e016ddf8cc77c74a751d99bd41a0fc
Grenkin1988 <kruglovgleb@gmail.com>
2021-06-23 18:03:29 +0200

    implicit

---
+++ src/main/scala/Implicits.scala
@@ -0,0 +1,20 @@
+case class Currency(code: String, amount: Double, toUSD: Double)
+
+object Implicits extends App {
+    implicit def stringToCurrency(money: String) = {
+        val Array(code: String, value: String) = money.split("\\s")
+        val ammountAsDouble = value.toDouble
+        code match {
+            case "USD" => Currency("USD", ammountAsDouble, ammountAsDouble)
+            case "NZD" => Currency("USD", ammountAsDouble, (ammountAsDouble * (1/1.5)))
+            case "CAD" => Currency("USD", ammountAsDouble, (ammountAsDouble * (1/1.3)))
+        }
+    }
+
+    println(stringToCurrency("USD 100"))
+    println(stringToCurrency("NZD 100"))
+    println(stringToCurrency("CAD 100"))
+
+    val cad: Currency = "CAD 100"
+    println(cad)
+}
